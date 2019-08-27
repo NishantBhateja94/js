feae51df6035fa09e6b5a3b7a91b24ad5ac9cab0
Nishant Bhateja <nishant.bhateja1994@gmail.com>
2019-08-28 00:07:01 +0530

    Create config2.js

---
+++ config2.js
@@ -0,0 +1,25 @@
+ {
+ "products":[{
+ "productId":2,
+        "productName":"Garden Cart",
+        "productCode":"GDN-0023",
+        "releaseDate":"March 18,2019",
+        "description":"15 gallon capacity rolling garden",
+        "price":32.99,
+         "starRating":4.2,
+         "imageUrl": './images/garden_cart.jpg'
+      },
+      {
+        "productId":5,
+        "productName":"Hammer",
+        "productCode":"try-0023",
+        "releaseDate":"March 20,2019",
+        "description":"15 Curved claw steel hammer",
+        "price":8.9,
+         "starRating":4.8,
+         "imageUrl":"./images/hammer.png"
+         
+         
+  }]
+  }
+         
